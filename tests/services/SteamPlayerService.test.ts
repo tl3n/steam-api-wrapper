942b5e5f8a5355e0e725ce30ecf022eec3359d3d
Vasyl <vasyl.pavlyuchenko@gmail.com>
2025-02-17 18:25:29 +0000

    Created player service tests

---
+++ tests/services/SteamPlayerService.test.ts
@@ -0,0 +1,28 @@
+import { describe, test, expect } from "@jest/globals";
+import { SteamClient } from "../../src/SteamClient";
+import { SteamPlayerService } from "../../src/services/SteamPlayerService";
+
+describe("SteamPlayerService", () => {
+  const steamClient = new SteamClient("API_KEY");
+  const playerService = new SteamPlayerService(steamClient);
+
+  test("should return player's owned games", async () => {
+    const response = await playerService.GetOwnedGames({
+      steamid: "76561197960434622",
+    });
+
+    expect(response.response).toBeDefined();
+    expect(response.response.game_count).toBe(1073);
+    expect(response.response.games.length).toBeGreaterThan(0);
+  });
+
+  test("should retrun player's recently played games", async () => {
+    const response = await playerService.GetRecentlyPlayedGames({
+      steamid: "76561197960434622",
+    });
+
+    expect(response.response).toBeDefined();
+    expect(response.response.total_count).toBeGreaterThanOrEqual(0);
+    expect(response.response.games.length).toBeGreaterThanOrEqual(0);
+  });
+});
